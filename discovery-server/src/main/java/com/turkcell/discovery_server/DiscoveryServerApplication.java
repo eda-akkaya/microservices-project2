57432fe04095f935126793b255b22d1d8bc1445d
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-10-19 11:08:53 +0300

    Enable Eureka Server in DiscoveryServerApplication for service discovery functionality

--- discovery-server/src/main/java/com/turkcell/discovery_server/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/turkcell/discovery_server/DiscoveryServerApplication.java
@@ -2,8 +2,10 @@ package com.turkcell.discovery_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;	
 
 @SpringBootApplication
+@EnableEurekaServer // Eureka Server'ın etkinleştirilmesi
 public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
