e232cb5e1e9e539fdeae5ba415307600e0a29368
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-11-05 16:40:38 +0300

    Refactor OrdersController to implement createOrder method and update API endpoint for order creation, enhancing RESTful service structure.

--- order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
+++ order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
@@ -1,13 +1,21 @@
 package com.turkcell.order_service.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
-@RequestMapping()
+@RequestMapping("/api/v1/orders")
 public class OrdersController {
     @PostMapping()
-    public String postMethodName(@RequestBody CreateOrderDto dto) {
+    public String createOrder(@RequestBody CreateOrderDto dto) {
+        // kafka eventi fırlat
+
         return dto.productId;
     }
 
+    // mimari açıdan doğru değil örnek olması için burada
     record CreateOrderDto(String productId) {
     }
 
