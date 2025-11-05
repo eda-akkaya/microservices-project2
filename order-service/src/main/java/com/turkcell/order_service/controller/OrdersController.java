ba66258fcc342ae80c8e31ce4df24ecb5a87bdb6
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-11-05 16:59:25 +0300

    Integrate Kafka messaging in OrdersController by adding StreamBridge for order creation events, enhancing communication capabilities.

--- order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
+++ order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
@@ -1,5 +1,6 @@
 package com.turkcell.order_service.controller;
 
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,15 +9,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/orders")
 public class OrdersController {
+
+    // kafka ile iletişim kurmamızı sağlıyor
+    private final StreamBridge streamBridge;
+
+    public OrdersController(StreamBridge streamBridge) {
+        this.streamBridge = streamBridge;
+    }
+
     @PostMapping()
     public String createOrder(@RequestBody CreateOrderDto dto) {
         // kafka eventi fırlat
-
+        OrderCreatedEvent event = new OrderCreatedEvent(dto.productId());
+        streamBridge.send("orderCreated-out-0", event);
         return dto.productId;
     }
 
     // mimari açıdan doğru değil örnek olması için burada
     record CreateOrderDto(String productId) {
+    }
+
+    record OrderCreatedEvent(String productId) {
     }
 
 }
